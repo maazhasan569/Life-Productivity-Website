c0e6b3043d4a74c4f526f564c1e6d6335eb842c9
Maaz hasan <maazhasan569@gmail.com>
2026-07-28 21:52:15 +0500

    feat: enhance users model with password hashing and JWT token generation methods

--- Backend/src/models/users.model.js
+++ Backend/src/models/users.model.js
@@ -1,29 +1,67 @@
 import mongoose from "mongoose"
-
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 const usersSchema = new mongoose.Schema({
 
-    username : {
-        type : String,
-        unique : true,
+    username: {
+        type: String,
+        unique: true,
     },
-    password : {
-        type : String,
-        unique : true
+    password: {
+        type: String,
+        unique: true
     },
-    email : {
-        type : String,
-        unique : true
+    email: {
+        type: String,
+        unique: true
     },
-    avatar : {
-        type : String,
-        required : true,
+    avatar: {
+        type: String,
+        required: true,
     },
 
-    refreshToken : {
-        type : String,
+    refreshToken: {
+        type: String,
     },
-    googleRefreshToken : {
-        type : String
+    googleRefreshToken: {
+        type: String
     }
 },
     { timestamps: true })
+
+usersSchema.pre("save", async function () {
+    if (!this.isModified("password")) return;
+    await bcrypt.hash(this.password, 10)
+})
+
+usersSchema.method.isPasswordValid = async function (password) {
+    await bcrypt.compare(password, this.password)
+}
+
+usersSchema.method.generateAccessToken = function () {
+
+    return jwt.sign(
+        {
+            username: this.username,
+            password: this.password,
+            email: this.email,
+            avatar: this.avatar,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+usersSchema.method.generateRefreshToken = function () {
+
+    return jwt.sign(
+        {
+            userId: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
