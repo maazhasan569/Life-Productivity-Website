ce9938a4ae2d6660b39ed4231e4db54fe1997553
Maaz hasan <maazhasan569@gmail.com>
2026-07-30 20:18:47 +0500

    feat: refactor Google user authentication flow with improved token handling and error management . add login functionality (google oAuth)

--- Backend/src/controllers/user/oAuth2.controller.js
+++ Backend/src/controllers/user/oAuth2.controller.js
@@ -6,6 +6,26 @@ import ApiError from "../../utils/ApiError"
 import ApiResponse from "../../utils/ApiResponse"
 import { generateAccessAndRefreshToken } from "../../utils/generateJwtToken"
 
+const getGoogleTokenAndPayload = async (code) => {
+
+    try {
+        if (!code) {
+            throw new ApiError(401, "auth code not found")
+        }
+        const { tokens } = await oauthClient.getToken(code)
+        oauthClient.setCredentials(tokens)
+        const ticket = oauthClient.verifyIdToken(
+            {
+                idToken: tokens._id.token,
+                audience: process.env.CLIENT_ID
+            }
+        )
+        const payload = ticket.getPayload()
+    } catch (err) {
+        throw new ApiError(401, "unauthorized token or expired")
+    }
+    return { payload, refresh_token: tokens.refresh_token, access_token: tokens.access_token }
+}
 const oauthClient = new oAuth2Client(
     process.env.CLIENT_ID,
     process.env.CLIENT_SECRET,
@@ -33,48 +53,65 @@ const loginUrl = asyncHandler((req, res) => {
 
 const registorGoogleUser = asyncHandler((req, res, next) => {
     const { code } = req.query;
-    if (!code) {
-        throw new ApiError(401, "auth code not found")
-    }
-    const { tokens } = await oauthClient.getToken(code)
-    const {access_token, refresh_token} = tokens
-    oauthClient.setCredentials(tokens)
-    const ticket = oauthClient.verifyIdToken(
-        {
-            idToken: tokens._id.token,
-            audience: process.env.CLIENT_ID
-        }
-    )
-
-    const payload = ticket.getPayload()
-    const isUserExist = await Users.find({googleId : payload.sub})
-    if(isUserExist){
+    const { payload, refresh_token, access_token } = await getGoogleTokenAndPayload(code)
+    const isUserExist = await Users.find({ googleId: payload.sub })
+    if (isUserExist) {
         return res.status(204).json(
-            new ApiResponse(204 , "user already exists")
+            new ApiResponse(204, "user already exists")
         )
     }
-
-    const {accessToken , refreshToken} = await generateAccessAndRefreshToken(createdUser._id)
     const createdUser = await Users.create({
-        googleId : payload.sub,
-        username : payload.name,
-        email : payload.email,
-        googleRefreshToken : refresh_token,
-        refreshToken : refreshToken
+        googleId: payload.sub,
+        username: payload.name,
+        email: payload.email,
+        googleRefreshToken: refresh_token,
     })
-
-    if(!createdUser){
-        throw new ApiError(500 , "failed to create the user" )
+    const { accessToken, refreshToken } = await generateAccessAndRefreshToken(createdUser._id)
+    if (!createdUser) {
+        throw new ApiError(500, "failed to create the user")
     }
     return res.status(201)
-    .cookie("accessToken" , accessToken , options)
-    .cookie("refreshToken" , refreshToken , options)
-    .json(
-        new ApiResponse(201 ,"user created", {createdUser , googleAccessToken : access_token , accessToken })
-    )
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
+        .json(
+            new ApiResponse(201, "user created", { createdUser, googleAccessToken: access_token, accessToken })
+        )
+
+})
+
+const loginGoogleUser = asyncHandler(async (req, res) => {
+    //get the auth code
+    //apply token func
+    //check if account exists
+
+    const { code } = req.query;
+    const { payload, refresh_token, access_token } = await getGoogleTokenAndPayload(code)
+    const user = await Users.findOne({ googleId: payload.sub })
+    if (!user) {
+        throw new ApiError(404, "User account not found")
+    }
+    const { accessToken, refreshToken } = await generateAccessAndRefreshToken(user._id)
+    user.googleRefreshToken = refresh_token
+    await user.save({ validiateBeforeSave: false })
+
+    return res.status(200)
+        .cookie(
+            "accessToken", accessToken, options
+        )
+        .cookie(
+            "refreshToken", refreshToken, options
+        ).json(
+            new ApiResponse(200, "user logged in successfully", {
+                loggedInUser: user,
+                accessToken,
+                googleAccessToken: access_token
+            })
+        )
+
 
 })
 export {
-    oauthClient,
-    loginUrl
+    loginUrl,
+    loginGoogleUser,
+    registorGoogleUser
 }
