aba1289192a7ae3ccda8de97341c3980396a4581
Maaz hasan <maazhasan569@gmail.com>
2026-08-04 21:19:01 +0500

    feat: implement multer middleware for file uploads and add file upload utility function

---
+++ Backend/src/utils/fileUpload.js
@@ -0,0 +1,30 @@
+import { v2 as cloudinary } from "cloudinary"
+import fs from "fs"
+
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET 
+})
+const fileUpload = async (filePath) => {
+    try {
+        // Configuration
+        if (!filePath) return null
+
+
+        const upload = await cloudinary.uploader.upload(filePath, {
+            resource_type: "auto",
+        })
+
+    
+        return upload.url
+    } catch (err) {
+        if (fs.existsSync(filePath)) {
+            fs.unlinkSync(filePath)
+        }
+        console.log(`cloudinary file upload failed , ${err}`)
+        return null
+    }
+}
+export default fileUpload
