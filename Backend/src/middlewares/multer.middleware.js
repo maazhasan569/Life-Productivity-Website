41244cf906838233b5369832396ff35a580cf8ea
Maaz hasan <maazhasan569@gmail.com>
2026-08-06 19:37:10 +0500

    feat: create file upload setup . filter and validate unwanted files

--- Backend/src/middlewares/multer.middleware.js
+++ Backend/src/middlewares/multer.middleware.js
@@ -1,16 +1,37 @@
 import multer from "multer"
-
+import ApiError from "./ApiError.js"
+import sharp from "sharp"
+import { inspectPdf } from "../utils/inspectPdf.js"
 const storage = multer.diskStorage({
-    destination : function (req , file , cb) {
-        cb(null , "./public/temp") 
+    destination: function (req, file, cb) {
+        cb(null, "./public/temp")
     },
-    filename : function(req, file , cb) {
+    filename: function (req, file, cb) {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-        
-        cb(null , file.fieldname + '-' + uniqueSuffix)
+
+        cb(null, file.fieldname + '-' + uniqueSuffix)
     }
 })
+const fileFilter = (req, file, cb) => {
+    try {
+        if (file.mimetype === "application/pdf" || file.mimetype.startsWith("image/")) {
+            cb(null, true)
+        } else {
+            cb(new ApiError(400, "A pdf or an imag file is required"), false)
+        }
+        const allowedFileExts = ['.jpg', '.jpeg', '.png', '.gif', '.webp', '.pdf']
+
+        const ext = file.originalname.substring(file.originalname.lastIndexOf('.')).toLowerCase();
+        if (!allowedFileExts.includes(ext)) {
+            cb(new ApiError(400, "File extension not allowed"), false)
+        }
+
+    } catch (err) {
+        cb(err.msg, false)
+    }
+}
 
 export const upload = multer({
     storage,
+
 })
