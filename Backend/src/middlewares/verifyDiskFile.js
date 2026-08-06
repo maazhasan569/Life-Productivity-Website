ae97694147676bd623aa3048b3a5a721477c3ba0
Maaz hasan <maazhasan569@gmail.com>
2026-08-06 20:11:06 +0500

    feat: add verifyDiskFile middleware for file type validation and error handling

---
+++ Backend/src/middlewares/verifyDiskFile.js
@@ -0,0 +1,27 @@
+import { fileTypeFromFile } from "file-type";
+import ApiError from "../utils/ApiError";
+import asyncHandler from "../utils/asyncHandler";
+import fs from "fs"
+const verifyDiskFile = asyncHandler(async (req, res, next) => {
+
+    if (!req.file) {
+        throw new ApiError(404, "No file given")
+    }
+    try {
+
+        const allowedTypes = ['image/jpeg', 'image/png', 'image/gif', 'application/pdf']
+        const detectedType = await fileTypeFromFile(req.file.path)
+
+        if (!(detectedType || allowedTypes.includes(detectedType.mime))) {
+            throw new ApiError(400, "Invalid file type")
+            fs.unlink(req.file.path)
+        }
+        req.file.realMimeType = detectedType.mime;
+        req.file.realExtension = detectedType.ext;
+        req.filePath = req.file.path
+
+        next()
+    } catch (err) {
+        throw new ApiError(500, err.msg)
+    }
+})
