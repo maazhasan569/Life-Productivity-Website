405ccd0837328ac35623cf45ae3de438057f293c
Maaz hasan <maazhasan569@gmail.com>
2026-08-12 21:10:16 +0500

    refactor: streamline createExpense method by removing budget checks and direct database interaction

--- Backend/src/controllers/budget/expense.controller.js
+++ Backend/src/controllers/budget/expense.controller.js
@@ -3,6 +3,7 @@ import asyncHandler from "../../utils/asyncHandler"
 import { Users } from "../../models/users.models"
 import { Expense } from "../../models/budget/expense.models"
 import ApiResponse from "../../utils/ApiResponse"
+import { ExpenseTracker } from "../../service/expense.service"
 const fieldCheck = (arr) => {
     arr.some((fields) => {
         return fields === "" || fields.trim() === ""
@@ -23,26 +24,8 @@ const createExpense = asyncHandler(async (req, res) => {
     }
     const user = await Users.findById(req.user._id)
     const userBudget = user.budget
-    if (userBudget === 0) {
-        throw new ApiError(400, "no budget amt left")
-    }
-    if (amount > userBudget) {
-        throw new ApiError(400, "Expense amount is to big , greator than budget")
-    }
-
-    const createExpense = await Expense.create(
-        {
-            userId: req.user._id,
-            amount,
-            category,
-        }
-    )
-
-    if (!createExpense) {
-        throw new ApiError(400, "failed to save expense in database")
-    }
-    user.budget -= amount
-    await user.save({ validiationBeforeSave: false })
+    const manageExpense = new ExpenseTracker(userBudget , req.user._id)
+    const createExpense = await manageExpense.addExpense(name , category , amount)
 
     return res.status(200)
         .json(
@@ -60,28 +43,8 @@ const editExpense = asyncHandler(async (req, res) => {
     }
     const user = await Users.findById(req.user._id)
     const userBudget = user.budget
-    if (userBudget === 0) {
-        throw new ApiError(400, "no budget amt left")
-    }
-    if (amount > userBudget) {
-        throw new ApiError(400, "Expense amount is to big , greator than budget")
-    }
-
-    const updateExpense = await Expense.findByIdAndUpdate(
-        expenseId,
-        {
-            amount,
-            category,
-            name,
-        },
-        { new: true }
-    )
-
-
-
-    if (!updateExpense) {
-        throw new ApiError(400, "failed to edit expense ")
-    }
+    const expense = new ExpenseTracker(userBudget , req.user._id)
+    const updateExpense = await expense.editExpense(expenseId, name , category , amount)
 
     return res.status(200)
         .json(
