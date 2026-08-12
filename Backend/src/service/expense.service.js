ad2b7690cd313f1e4e05ea6d35f1d8cb58adf346
Maaz hasan <maazhasan569@gmail.com>
2026-08-12 09:38:02 +0500

    feat: implement ExpenseTracker class with methods for adding and editing expenses, and calculating budget

---
+++ Backend/src/service/expense.service.js
@@ -0,0 +1,57 @@
+import ApiError from "../utils/ApiError"
+import { Expense } from "../models/budget/expense.models"
+import { Users } from "../models/users.models"
+export class ExpenseTracker {
+    constructor(budget, userId) {
+        this.budget = budget,
+            this.expense = expense,
+            this.userId = userId
+    }
+    addExpense(name, category = null, amount) {
+        if (amount > this.budget || amount <= 0) {
+            throw new ApiError(400, "Invalid budget")
+        }
+        const createExpense = await Expense({
+            userId,
+            amount,
+            category
+        })
+        if (!createExpense) {
+            throw new ApiError(500, "failed to save expense in Db")
+        }
+        return createExpense
+    }
+    editExpense(name, category = null, amount) {
+        if (amount > this.budget || amount <= 0) {
+            throw new ApiError(400, "Invalid budget")
+        }
+        const updateUserExpense = await Expense.findByIdAndUpdate(
+            this.userId,
+            {
+                amount,
+                category,
+                name
+            }
+        )
+        if (!updateUserExpense) {
+            throw new ApiError(500, "Failed to edit user expense")
+        }
+        return updateUserExpense
+    }
+    getTotalSpend() {
+        return this.expenses.reduce((total, exp) => total + exp.amount, 0);
+    }
+    getAndSaveRemainingBudget() {
+        const user = await Users.findByIdAndUpdate(
+            this.userId,
+            {
+                budget: this.budget - this.getTotalSpend
+            },
+            { new: true }
+        )
+        if (!user) {
+            throw new ApiError(500, "Failed to update user budget")
+        }
+        return user.budget
+    }
+}
