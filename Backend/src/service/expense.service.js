e482d94712c5ab9718ad42b4c9f20d568b49c789
Maaz hasan <maazhasan569@gmail.com>
2026-08-12 09:51:54 +0500

    feat: refactor addExpense and editExpense methods for improved validation and error handling

--- Backend/src/service/expense.service.js
+++ Backend/src/service/expense.service.js
@@ -6,58 +6,71 @@ export class ExpenseTracker {
         if (!this.budget || this.budget <= 0) {
             throw new ApiError(400, "Invalid budget")
         }
-        if(!userId){
-            throw new ApiError(400 , "No user Id")
+        if (!userId) {
+            throw new ApiError(400, "No user Id")
         }
         this.budget = budget,
             this.userId = userId
 
     }
-    addExpense(name, category = null, amount) {
-        if (amount > this.budget || amount <= 0) {
-            throw new ApiError(400, "Invalid budget")
+    async addExpense(name, category = "General", amount) {
+        if (!amount || amount <= 0) {
+            throw new ApiError(400, "Invalid expense amount")
         }
-        const createExpense = await Expense({
-            userId,
-            amount,
-            category
-        })
-        if (!createExpense) {
-            throw new ApiError(500, "failed to save expense in Db")
+
+        if (amount > this.budget) {
+            throw new ApiError(400, "Expense amount exceeds budget")
+        }
+        try {
+            const createExpense = await Expense({
+                userId: this.userId,
+                amount,
+                category,
+                name,
+            })
+        } catch (error) {
+            throw new ApiError(500, error.msg)
         }
         return createExpense
     }
-    editExpense(name, category = null, amount) {
-        if (amount > this.budget || amount <= 0) {
+    async editExpense(name, category = "General", amount) {
+        if (!amount || amount <= 0) {
             throw new ApiError(400, "Invalid budget")
         }
-        const updateUserExpense = await Expense.findByIdAndUpdate(
-            this.userId,
-            {
-                amount,
-                category,
-                name
-            }
-        )
-        if (!updateUserExpense) {
-            throw new ApiError(500, "Failed to edit user expense")
+
+        if (amount > this.budget) {
+            throw new ApiError(400, "Expense amount exceeds budget")
+        }
+        try {
+            const updateUserExpense = await Expense.findByIdAndUpdate(
+                this.userId,
+                {
+                    amount,
+                    category,
+                    name
+                }
+            )
+
+        } catch (error) {
+            throw new ApiError(500, error.msg)
         }
         return updateUserExpense
     }
     getTotalSpend() {
         return this.expenses.reduce((total, exp) => total + exp.amount, 0);
     }
-    getAndSaveRemainingBudget() {
-        const user = await Users.findByIdAndUpdate(
+    async getAndSaveRemainingBudget() {
+        try{
+            const user = await Users.findByIdAndUpdate(
             this.userId,
             {
                 budget: this.budget - this.getTotalSpend
             },
             { new: true }
         )
-        if (!user) {
-            throw new ApiError(500, "Failed to update user budget")
-        }
+    }catch(error){
+        throw new ApiError(500 , error.msg)
+    }
         return user.budget
     }
 }
