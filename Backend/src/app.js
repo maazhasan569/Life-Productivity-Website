b21cfbed338d0b991da2fcff2b19749ea5c88570
Maaz hasan <maazhasan569@gmail.com>
2026-08-28 18:17:48 +0500

    feat: add goal router to API for goal management

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -19,9 +19,11 @@ app.use(cookieParser())
 import authRouter from "./routes/user/auth.route.js"
 import userRouter from "./routes/user/user.route.js"
 import expenseRouter from "./routes/budget/expense.route.js"
+import goalRouter from "./routes/budget/goal.route.js"
 app.use("/api/v1/auth" , authRouter)
 app.use("/api/v1/user" , userRouter)
 app.use("/api/v1/expenses", expenseRouter)
+app.use("/api/v1/goals",goalRouter)
 app.use((err, req, res, next) => {
     let statusCode = err.statusCode || 500;
     let message = err.message || "Internal Server Error";
